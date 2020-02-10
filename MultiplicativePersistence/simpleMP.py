59c924b6d8853383648d4159662a5ef281dffe67
BharathC15 <bharathcb@yahoo.in>
2020-02-10 11:52:43 +0530

    Working simple persistence code

--- MultiplicativePersistence/simpleMP.py
+++ MultiplicativePersistence/simpleMP.py
@@ -1,3 +1,18 @@
 # Bharath
-
-print("Hello world")
+# Try with the number 277777788888899
+import os
+os.system('cls||clear')
+number=int(input("Enter a number :"))
+print("The entered number is :",number)
+def persistant(n):
+    step=0
+    while len(str(n))>1:
+        final=1
+        step+=1
+        m=[int(x) for x in str(n)]
+        for i in m:
+            final*=i
+        print("Step no",step,"Value is",final)
+        n=final 
+    print("Total No of steps is :",step)
+persistant(number)
